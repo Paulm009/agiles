ccf4e10e0c66025817ec88a1f790bf7b1d9c567c
Paulm009 <poolpos8@gmail.com>
2023-03-24 08:49:28 -0400

    la vida

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<link href="/css/style.css" rel="stylesheet">
+
+    <header class="header">
+        <div class="logo">
+            <h1>Gestion de hoteles</h1>
+        </div>
+        <nav>
+           <ul class="nav-links">
+                <li><a href="#">HOME</a></li>
+                <li><a href="#">RESERVAS</a></li>
+                <li><a href="#">LISTA DE HABITACIONES</a></li>
+           </ul>            
+        </nav>
+        
+    </header>
+  
+    <body>
+    
+    <h1 class="title">LISTA DE HABITACIONES</h1>
+<br><br>
+    <div class="container__slider">
+
+        <div class="container">
+            <input type="radio" name="slider" id="item-1" checked>
+            <input type="radio" name="slider" id="item-2">
+            <input type="radio" name="slider" id="item-3">
+
+            <div class="cards">
+                <label class="card" for="item-1" id="selector-1">
+                <img src="{{ asset('habi.jpg') }}" alt="Descripción de la imagen">
+
+                </label>
+                <label class="card" for="item-2" id="selector-2">
+                <img src="{{ asset('HABI2.jpg') }}" alt="Descripción de la imagen">
+                </label>
+                <label class="card" for="item-3" id="selector-3">
+                <img src="{{ asset('HABI3.jpg') }}" alt="Descripción de la imagen">
+                </label>
+
+            </div>
+        </div>
+
+    </div>
+
+</body>
+
+
+
+</html>
