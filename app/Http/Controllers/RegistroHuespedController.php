dd77fb790e101c1015421680152a3a1708b43a7c
Gomer Claure <gomerclaureb@gmail.com>
2023-04-04 23:00:06 -0400

    Cambiando nombre de controlador huesped

--- app/Http/Controllers/RegistroHuespedController.php
+++ app/Http/Controllers/RegistroHuespedController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class RegistroHabitacionController extends Controller
+class RegistroHuespedController extends Controller
 {
     public function index(Request $request)
     {
