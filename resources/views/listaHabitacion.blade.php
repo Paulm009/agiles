0ce29d7db0f5b92e594fd1cf4812c02926474daa
Evelin Viscarra <evelin.viscarra@gmail.com>
2023-04-05 15:33:57 -0400

    Agregando boostrap a la vista de lista

--- resources/views/listaHabitacion.blade.php
+++ resources/views/listaHabitacion.blade.php
@@ -1,48 +1,27 @@
 <!DOCTYPE html>
 <html lang="es">
-    <head>
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Hotel Dunas</title>
-        <!-- Bootstrap -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        <!-- Custom CSS -->
-        <!--link rel="stylesheet" href="css/index.css"/-->
-        <link rel="stylesheet" href="{{ asset('css/listaHabitacion.css') }}">
-    </head>
+<header class="header">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>        <link href="/css/nav.css" rel="stylesheet">
+    <div class="logo">
+      <h1 class="text-light">Gestion de hoteles</h1>
+    </div>
+    <nav class="navbar-expand-sm menu">
+      <ul class="nav-links">
+        <li><a href="{{url('/')}}">HOME</a></li>
+        <li><a href="{{url('/reserva')}}">RESERVAS</a></li>
+        <li><a href="{{url('/listaHabitacion')}}">LISTA DE HABITACIONES</a></li>
+      </ul>            
+    </nav>
+</header>
 
-    <body>
-        <nav class="navbar">
-            <div class="container-nav">
-                <h1 class="titulo">Las Dunas</h1>
-            </div>
-        </nav>
-
-        <div class="container p-5" id = "container">
-            <h2 class = "list_hab">Lista Habitaciones</h2>
-            <div class = "input-container">
-                
-                <label class="text-white" id = "title_search">Buscar: </label>
-    
-                <select class = "combo_box" id = "title_search">
-                    <option value="opcion1">Simple</option>
-                    <option value="opcion2">Doble</option>
-                    <option value="opcion3">Triple</option>
-                    <option value="opcion3">Disponible</option>
-                    <option value="opcion3">Ocupado</option>
-                </select>
-    
-                <button class = "boton">Buscar
-                    <!--img src="img/search.svg" alt="search icon"-->
-                </button>
-
-            </div>
-
+<body>
+  <div class="container p-5" id = "container" >
+      <h2 class="list_hab text-center">Lista Habitaciones</h2>
             <table class="table mt-5" id = "tabla">
                 <thead>
                   <tr id = "first_column">
-                    <th scope="col">#</th>
+                    <th scope="col">Nro</th>
                     <th scope="col">Nombre</th>
                     <th scope="col">Tipo</th>
                     <th scope="col">Capacidad</th>
@@ -108,7 +87,7 @@
                 </tbody>
               </table>
         </div>
-        <div class = "botones">
+        <div class="botones">
             <button type="button" class="btn btn-lg">Nuevo</button>
             <button type="button" class="btn btn-lg">Salir</button>
         </div>
