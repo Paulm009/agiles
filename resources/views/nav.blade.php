bee028b4c4320903f51eb255b22c867caa81893e
Abi02GB <abigailgalzin@gmail.com>
2023-06-22 19:56:56 -0400

    merge

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -3,32 +3,8 @@
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="{{url('/')}}">Inicio</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="{{url('/reserva')}}">Reservas</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="{{url('/listaHabitacion')}}">Lista de Habitaciones</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="{{url('/registro/huesped')}}">Registro</a>
-        </li>
-        </ul>
-    </div>
-    <div class="text-center w-100 col">
-        <a class="navbar-brand logo" href="{{url('/')}}">Gestion de Hoteles</a>
-    </div>
-    </div>
-</nav> --}}
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container-fluid">       
-        
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
                     <a class="nav-link" href="{{url('/')}}">Inicio</a>
                 </li>
