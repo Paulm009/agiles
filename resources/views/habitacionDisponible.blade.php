76b56940821a00081462bf716afe7861b55594d2
esther-romero <esther.romeagui@icloud.com>
2023-06-23 00:51:57 -0400

    [UPDATE] design

--- resources/views/habitacionDisponible.blade.php
+++ resources/views/habitacionDisponible.blade.php
@@ -8,7 +8,7 @@
   
     @extends('layout')
     @section('content')
-        <div class="container p-2">
+        <div class="container p-3">
             <div class="hero">
                 <h1 class="title text-center mt-4 mb-5"> Lista de Habitaciones <br> Disponibles </h1>
             </div>
@@ -18,22 +18,18 @@
                 <div class="col-md-4">
                     <div class="card mb-3">
                         <div class="row g-0">
-                            <div class="col-md-4">
-                                <img src="{{ asset('redi.jpg') }}" class="img-fluid rounded-start w-100" alt="...">
-                            </div>
-                            
-                            <div class="col-md-8">
-                                <div class="card-body">
-                                    <h5 class="card-title">{{$habitaciones->nombreHabitacion}}</h5>
+                            <img src="{{ asset('tres.jpg') }}" class="img-fluid rounded-start w-100" alt="...">
 
-                                    <p class="card-text ">{{$habitaciones->descripcion}}</p>
+                            <div class="card-body">
+                                <h5 class="card-title">{{$habitaciones->nombreHabitacion}}</h5>
 
-                                    <p class="text-center card-text"><small class="text-body-secondary">Precio: {{$habitaciones->precio}}</small></p>
-                                    
-                                    <p class="text-center card-text"><small class="text-body-secondary">Tipo de habitación: {{$habitaciones->tipoHabitacion->tipo}}</small></p>
+                                <p class="card-text ">{{$habitaciones->descripcion}}</p>
 
-                                    <a type="button" href="#" class="btn w-100 btn-warning mt-2">Reservar</a>
-                                </div>
+                                <p class="text-center card-text"><small class="text-body-secondary">Precio: {{$habitaciones->precio}}</small></p>
+                                
+                                <p class="text-center card-text"><small class="text-body-secondary">Tipo de habitación: {{$habitaciones->tipoHabitacion->tipo}}</small></p>
+
+                                <a type="button" href="#" class="btn w-100 btn-warning mt-2">Reservar</a>
                             </div>
                         </div>
                     </div>
